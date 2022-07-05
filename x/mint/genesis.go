19053f20ba634b766c1831bb27c17bab85738a04
jay tseng <jay.tseng@crypto.com>
2022-07-05 12:59:38 -0700

    move ExportGenesisTo from mint/module to mint/genesis

--- x/mint/genesis.go
+++ x/mint/genesis.go
@@ -1,6 +1,9 @@
 package mint
 
 import (
+	"os"
+	"path"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/mint/keeper"
 	"github.com/cosmos/cosmos-sdk/x/mint/types"
@@ -18,4 +21,34 @@ func ExportGenesis(ctx sdk.Context, keeper keeper.Keeper) *types.GenesisState {
 	minter := keeper.GetMinter(ctx)
 	params := keeper.GetParams(ctx)
 	return types.NewGenesisState(minter, params)
+}
+
+func ExportGenesisTo(ctx sdk.Context, k keeper.Keeper, exportPath string) error {
+	if err := os.MkdirAll(exportPath, 0755); err != nil {
+		return err
+	}
+
+	filePath := path.Join(exportPath, "genesis0")
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	gs := ExportGenesis(ctx, k)
+	if err != nil {
+		return err
+	}
+
+	bz, err := gs.Marshal()
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(bz)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
