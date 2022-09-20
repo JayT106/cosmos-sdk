39ac2ae4fdde6f96daa485acffc7371600c2e1ac
jay tseng <jay.tseng@crypto.com>
2022-09-20 16:41:52 -0400

    implement backendDB options into SDK as a cli flag

--- server/start.go
+++ server/start.go
@@ -66,6 +66,9 @@ const (
 	flagGRPCAddress    = "grpc.address"
 	flagGRPCWebEnable  = "grpc-web.enable"
 	flagGRPCWebAddress = "grpc-web.address"
+
+	// DBBackend options
+	FlagDbMaxfileOpen = "maxopenfiles"
 )
 
 // StartCmd runs the service passed in, either stand-alone or in-process with
@@ -165,6 +168,8 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 	cmd.Flags().Uint64(FlagStateSyncSnapshotInterval, 0, "State sync snapshot interval")
 	cmd.Flags().Uint32(FlagStateSyncSnapshotKeepRecent, 2, "State sync snapshot to keep")
 
+	cmd.Flags().Uint64(FlagDbMaxfileOpen, 0, "max open files allowed in the backend DB(0 means default settings in the backend DB)")
+
 	// add support for all Tendermint-specific command line options
 	tcmd.AddNodeFlags(cmd)
 	return cmd
@@ -175,7 +180,8 @@ func startStandAlone(ctx *Context, appCreator types.AppCreator) error {
 	transport := ctx.Viper.GetString(flagTransport)
 	home := ctx.Viper.GetString(flags.FlagHome)
 
-	db, err := openDB(home)
+	opts := createDbOptionsFromFlag(ctx)
+	db, err := openDBwithOptions(home, opts)
 	if err != nil {
 		return err
 	}
@@ -233,12 +239,13 @@ func startInProcess(ctx *Context, clientCtx client.Context, appCreator types.App
 		}
 	}
 
-	traceWriterFile := ctx.Viper.GetString(flagTraceStore)
-	db, err := openDB(home)
+	opts := createDbOptionsFromFlag(ctx)
+	db, err := openDBwithOptions(home, opts)
 	if err != nil {
 		return err
 	}
 
+	traceWriterFile := ctx.Viper.GetString(flagTraceStore)
 	traceWriter, err := openTraceWriter(traceWriterFile)
 	if err != nil {
 		return err
